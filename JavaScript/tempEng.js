908f1ce1a6d0d11ea452434c9a2f3073b4e28a67
Benny <158503969+OverlyEngineered@users.noreply.github.com>
2025-08-06 21:35:48 -0400

    Removed Reference
    Removed the commented out fetch code I got from the internet to use as a reference.

--- JavaScript/tempEng.js
+++ JavaScript/tempEng.js
@@ -1,18 +1,3 @@
-// fetch('https://api.example.com/data')
-//   .then(response => {
-//     if (!response.ok) {
-//       throw new Error('Network response was not ok');
-//     }
-//     return response.json(); // Returns a Promise
-//   })
-//   .then(data => {
-//     console.log('Fetched data:', data); // Processes the resolved JSON data
-//     // Further operations with 'data' can be performed here
-//   })
-//   .catch(error => {
-//     console.error('There was a problem with the fetch operation:', error);
-//   });
-
 async function getTemplate(tempFileName) {
     url = `/Templates/${tempFileName}`
 
