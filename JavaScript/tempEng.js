25ec91360d8d084bf3bc5a080ebbee8b2d4d0c0b
Benny <158503969+OverlyEngineered@users.noreply.github.com>
2025-08-07 15:36:27 -0400

    Begin De-functioning
    removed the code from function to make testing easier.

--- JavaScript/tempEng.js
+++ JavaScript/tempEng.js
@@ -1,5 +1,5 @@
 async function getTemplate(tempFileName) {
-    url = `/Templates/${tempFileName}`
+    url = `Templates/${tempFileName}`
 
     fetch(url)
         .then(
@@ -12,12 +12,6 @@ async function getTemplate(tempFileName) {
         )
         .then(
             data => {
-                console.log(data)
-                insertTemplate(data, "navbar-location")
-
-
-
-
                 return data; // Processes the resolved JSON data
                 // Further operations with 'data' can be performed here
             }
@@ -32,16 +26,18 @@ async function getTemplate(tempFileName) {
 // repository-templates
 
 
-function tagTemplate(html, tempId, repoId) {
-    const newTemplate = document.createElement("template");
-    newTemplate.content = html
-    newTemplate.id = tempId
-    const tempRepo = document.getElementById(repoId)
-    tempRepo.appendChild(newTemplate)
-}
 
 
 
+
+
+// function tempTagHTML(html, tempId, repoId) {
+//     const newTemplate = document.createElement("template");
+//     newTemplate.content = html
+//     newTemplate.id = tempId
+//     const tempRepo = document.getElementById(repoId)
+//     tempRepo.appendChild(newTemplate)
+// }
 
 function insertTemplate(template, designation){
     const designationElement = document.getElementById(designation)
@@ -49,5 +45,41 @@ function insertTemplate(template, designation){
     designationElement.innerHTML = html
 }
 
-getTemplate("navbar.html")
 
+async function tagTemplate(tempFileName, tempId, repoId) {
+    const templateHTML = await getTemplate(tempFileName).then();
+    console.log("templateHTML:", templateHTML)
+    const templateRepository = document.getElementById(repoId)
+
+    const newTemplate = document.createElement("template");
+    newTemplate.content = templateHTML
+    newTemplate.id = tempId
+    templateRepository.appendChild(newTemplate)
+}
+
+
+
+
+
+url = `Templates/${tempFileName}`
+
+fetch(url)
+    .then(
+        response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text(); // Returns a Promise
+        }
+    )
+    .then(
+        data => {
+            return data; // Processes the resolved JSON data
+            // Further operations with 'data' can be performed here
+        }
+    )
+    .catch(
+        error => {
+            console.error('There was a problem with the fetch operation:', error);
+        }
+    );  
