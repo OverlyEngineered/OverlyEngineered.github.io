79792e99c6a7a762f6470b231362f02b322bfbc7
Benny <158503969+OverlyEngineered@users.noreply.github.com>
2025-08-07 22:01:51 -0400

    Full Set of Templating Functions
    Made a full set of templating functions that can perform the tasks needed to make the engine work.
    Will likely change out functions with a class.
    
    Currently uses .innerhtml for the template tag which isn’t great.
    
    There is also alot of comments that need to be remmoved.

--- JavaScript/tempEng.js
+++ JavaScript/tempEng.js
@@ -1,32 +1,84 @@
-async function getTemplate(tempFileName) {
-    url = `Templates/${tempFileName}`
-
-    fetch(url)
-        .then(
-            response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.text(); // Returns a Promise
-            }
-        )
-        .then(
-            data => {
-                return data; // Processes the resolved JSON data
-                // Further operations with 'data' can be performed here
-            }
-        )
-        .catch(
-            error => {
-                console.error('There was a problem with the fetch operation:', error);
-            }
-        );  
+async function getTemplateHTML(tempFileName) {
+    const url = `Templates/${tempFileName}`
+    const response = await fetch(url)
+    if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+    }
+    const templateHTML = await response.text()
+    return templateHTML
 }
 
+async function tagTemplate(tempFileName, tempId, repoId) {
+    const templateHTML = await getTemplateHTML(tempFileName);
+
+    const templateRepository = document.getElementById(repoId)
+    const newTemplate = document.createElement("template");
+    
+    console.log("templateHTML:", templateHTML)
+    console.log("is string:", typeof templateHTML === "string")
+    newTemplate.innerHTML = templateHTML
+    newTemplate.id = tempId
+
+    console.log("newtemp:", newTemplate.content.children)
+
+    templateRepository.appendChild(newTemplate)
+}
+
+// async function insertTemplate(template, designation){
+//     const designationElement = document.getElementById(designation)
+//     const html = template;
+//     designationElement.innerHTML = html
+// }
+async function insertTemplate(template, designation){
+    const designationElement = document.querySelector(`[data-insert-template="${designation}"]`)
+    console.log(template)
+    const clon = document.getElementById(template).content.cloneNode(true);
+    designationElement.appendChild(clon)
+}
+
+async function findRequiredTemplates() {
+    const listRequiredTemplates = document.querySelectorAll("[data-insert-template]")
+
+    for (let RequiredTemplate of listRequiredTemplates) {
+        const templateFileName = RequiredTemplate.dataset.insertTemplate
+        const templateId = templateFileName.replace(".html", "")
+
+        await tagTemplate(templateFileName, templateId, "templates-repository")
+        // tagTemplate(tempFileName, tempId, repoId)
+        // insertTemplate(templateId, templateFileName)
+    }
+}
+findRequiredTemplates()
+
+
+
+
+
+
+
+
+//     fetch(url)
+//         .then(
+//             response => {
+//                 if (!response.ok) {
+//                     throw new Error('Network response was not ok');
+//                 }
+//                 return response.text(); // Returns a Promise
+//             }
+//         )
+//         .then(
+//             data => {
+//                 return data; // Processes the resolved JSON data
+//                 // Further operations with 'data' can be performed here
+//             }
+//         )
+//         .catch(
+//             error => {
+//                 console.error('There was a problem with the fetch operation:', error);
+//             }
+//         );  
+
 // repository-templates
-
-
-
 
 
 
@@ -39,47 +91,33 @@ async function getTemplate(tempFileName) {
 //     tempRepo.appendChild(newTemplate)
 // }
 
-function insertTemplate(template, designation){
-    const designationElement = document.getElementById(designation)
-    const html = template;
-    designationElement.innerHTML = html
-}
-
-
-async function tagTemplate(tempFileName, tempId, repoId) {
-    const templateHTML = await getTemplate(tempFileName).then();
-    console.log("templateHTML:", templateHTML)
-    const templateRepository = document.getElementById(repoId)
-
-    const newTemplate = document.createElement("template");
-    newTemplate.content = templateHTML
-    newTemplate.id = tempId
-    templateRepository.appendChild(newTemplate)
-}
 
 
 
 
 
-url = `Templates/${tempFileName}`
 
-fetch(url)
-    .then(
-        response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.text(); // Returns a Promise
-        }
-    )
-    .then(
-        data => {
-            return data; // Processes the resolved JSON data
-            // Further operations with 'data' can be performed here
-        }
-    )
-    .catch(
-        error => {
-            console.error('There was a problem with the fetch operation:', error);
-        }
-    );  
+
+// const tempFileName = "navbar.html"
+// const url = `Templates/${tempFileName}`
+
+// fetch(url)
+//     .then(
+//         response => {
+//             if (!response.ok) {
+//                 throw new Error('Network response was not ok');
+//             }
+//             return response.text(); // Returns a Promise
+//         }
+//     )
+//     .then(
+//         data => {
+//             return data; // Processes the resolved JSON data
+//             // Further operations with 'data' can be performed here
+//         }
+//     )
+//     .catch(
+//         error => {
+//             console.error('There was a problem with the fetch operation:', error);
+//         }
+//     );
